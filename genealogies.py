9499c67e03ba1ebf8372b124ae9b3eab5d18f216
Davide Riva <60431521+dave-shore@users.noreply.github.com>
2022-11-02 10:33:04 +0100

    Update genealogies.py

--- genealogies.py
+++ genealogies.py
@@ -1,47 +1,33 @@
 def genealogy_chains(d, layer = 1, upstream = []):
-  
-    '''
-    Function to retrieve genealogy chains in a dict-list mixed structure.
-    
-    :param d: dict, Genealogy data structure.
-    :param layer: int, Layer counter NOT TO BE CHANGED UNLESS YOU WANT THE COUNTER TO START FROM N > 1.
-    :param upstream: list, List of upstream nodes NOT TO BE CHANGED UNLESS YOU WANT TO INCLUDE NODES A-PRIORI.
-    
-    :return: list(list)
-    '''
     
     output = []
     
     for k,L in d.items():
         
-        upstream += [(k,layer)]
-        
         if len(L) == 0:
-            output.append(upstream)
+            output.append(upstream + [(k,layer)])
             continue
-            
-        if isinstance(L, list):
         
+        if isinstance(L, list):
             for x in L:
 
                 if isinstance(x, dict):
-                    output.extend(genealogy_chains(x, layer+1, upstream))
+                    output.extend(genealogy_chains(x, layer+1, upstream + [(k,layer)]))
 
                 elif isinstance(x, str) and "(C)" in x:
-                    output.append(upstream + [(x,layer)])
+                    output.append(upstream + [(k,layer),(x,layer)])
 
                 else:
                     pass
-                  
+        
         elif isinstance(L, dict):
-          
             for x in L.values():
 
-                if isinstance(x, dict) or is:
-                    output.extend(genealogy_chains(x, layer+1, upstream))
+                if isinstance(x, dict):
+                    output.extend(genealogy_chains(x, layer+1, upstream + [(k,layer)]))
 
                 elif isinstance(x, str) and "(C)" in x:
-                    output.append(upstream + [(x,layer)])
+                    output.append(upstream + [(k,layer),(x,layer)])
 
                 else:
                     pass
